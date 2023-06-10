0b5be07fc6b2b8dffb44cbe02a11f65cd1bbe1dd
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2023-06-10 10:03:06 -0500

    Configure webpack for TS and create basic Express server

---
+++ src/server/index.ts
@@ -0,0 +1,12 @@
+import express, {Express, Request, Response} from 'express'
+import { config } from './config';
+
+const app: Express = express();
+
+app.get('*', (req: Request, res: Response) => {
+  res.send(`<h1>Hola mundo con ruta: ${req.url}</h1>`)
+})
+
+app.listen(config.PORT, () => {
+  console.log(`Listening in http://localhost:${config.PORT}`)
+})
